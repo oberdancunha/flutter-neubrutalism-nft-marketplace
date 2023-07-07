e973e3800515ea7d93bd98a3f24953274dfe495e
oberdancunha <oberdancunha@gmail.com>
2023-07-07 13:55:07 -0300

    Set device orientation to portraitUp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app_widget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+    ],
+  );
   runApp(const AppWidget());
 }
